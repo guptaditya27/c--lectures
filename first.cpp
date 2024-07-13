1658daa2eeedb1d4f279f5c5284677847366085f
Aditya Gupta <adgupta285@gmail.com>
2024-07-13 05:45:02 +0530

    iniitial commit

---
+++ first.cpp
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("Enter a value");
+    scanf("%d",&x);
+    y=x*x;
+    printf("Square of %d is %d",x,y);
+    return 0;
+}
+/* cpp is superset of c*/
