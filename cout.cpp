cd2e7619e08fee75c172d7f90bdd7033335abeb5
Aditya Gupta <adgupta285@gmail.com>
2024-07-13 15:07:18 +0530

    namespace commit

--- cout.cpp
+++ cout.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
+using namespace std;
 int main()
 {
     int x,y;
-    std::cout<<"ENter a Number:- ";
-    std::cin>>x;
+    //std::cout<<"ENter a Number:- ";
+    cout<<"Enter a Number";
+    cout<<endl;// for changing line same as \n;
+
+    //std::cin>>x;these are used if we are not declaring (namespace using keyword)
+    cin>>x;
     y=x*x;
-    std::cout<<"Square of "<<x <<" is " <<y;
+    //std::cout<<"Square of "<<x <<" is " <<y;
+    cout<<"Square of "<<x<<" is "<<y;
+    //cout<<"\n";
+    cout<<endl;// for changing line same as \n;
+    
+    
+    
+    
+    return 0;
 }
